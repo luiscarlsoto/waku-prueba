68de97448fb39de126753a714397dbb62c56cafd
Luis <Luiscarlsoto@gmail.com>
2021-01-14 13:10:27 -0400

    Archivos sin uso eliminados

--- cliente/src/routes/games/content/listGames.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const listGames = () => {
-    return (
-        <div>
-            games
-        </div>
-    )
-}
--- servidor/src/controllers/auth.controller.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response} from 'express';
-import * as jwt from "jwt-simple";
-
-const secret = process.env.JWT_SECRET as string;
-export const createJWTToken = (payload: any) => {
-    return jwt.encode(payload, secret, 'HS256');
-};
-
